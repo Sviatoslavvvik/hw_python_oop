567d3a656e8d54167ab9120ee6d0c9092adae9c0
Sviat_IK <Ikonnikov.svyat@gmail.com>
2022-02-12 19:01:53 +0300

    To fix for PEP8 compliance-fifth iteration

--- homework.py
+++ homework.py
@@ -146,9 +146,9 @@ def read_package(workout_type: str, data: list) -> Training:
                                    Running,
                                    SportsWalking]
                         ] = {'SWM': Swimming,
-                            'RUN': Running,
-                            'WLK': SportsWalking
-                            }
+                             'RUN': Running,
+                             'WLK': SportsWalking
+                             }
 
     return (workout_types[workout_type](*data))
 
@@ -161,10 +161,10 @@ def main(training: Training) -> None:
 
 if __name__ == '__main__':
     packages = [
-                 ('SWM', [720, 1, 80, 25, 40]),
+                ('SWM', [720, 1, 80, 25, 40]),
                  ('RUN', [15000, 1, 75]),
                  ('WLK', [9000, 1, 75, 180]),
-               ]
+                ]
 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
