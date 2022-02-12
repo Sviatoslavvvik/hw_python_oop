26af5e91c989b8cfaa993c35940170ed65c1a04f
Sviat_IK <Ikonnikov.svyat@gmail.com>
2022-02-12 19:15:29 +0300

    To fix for PEP8 compliance-seven  iteration

--- homework.py
+++ homework.py
@@ -160,11 +160,9 @@ def main(training: Training) -> None:
 
 
 if __name__ == '__main__':
-    packages = [
-                 ('SWM', [720, 1, 80, 25, 40]),
+    packages = [ ('SWM', [720, 1, 80, 25, 40]),
                  ('RUN', [15000, 1, 75]),
-                 ('WLK', [9000, 1, 75, 180]),
-               ]
+                 ('WLK', [9000, 1, 75, 180]),]
 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
